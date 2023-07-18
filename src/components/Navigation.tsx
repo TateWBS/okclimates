6f23e4862ffe36d8d3cb77fe5ea19b0ef2a9e9da
TateFowble <tatefowble@gmail.com>
2023-07-18 15:24:47 -0500

    added some more design for navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,8 +2,8 @@
 import Link from "next/link";
 import Style from '@styles/Navigation.module.scss';
 import backgroundImage from '@assets/images/okc.jpg';
-
-
+import Image from "next/image";
+import logo from '@assets/images/logo.png';
 
 const Navigation = () => {
 
@@ -11,7 +11,20 @@ const Navigation = () => {
         <nav
             className={Style.navigation}
             style={{ backgroundImage: `url(${backgroundImage.src})` }}>
-            dasds
+            <div className='d-flex justify-content-around'>
+                <ul>
+                    <Link href='#specials'><li>Featured Promotions</li></Link>
+                    <Link href='#core'><li>Our core values</li></Link>
+                    <Link href='#offers'><li>Our Offers</li></Link>
+                    <Link href='#contact'><li>Lets Chat</li></Link>
+
+
+                </ul>
+                <Image src={logo} alt='' />
+                <div className='d-flex align-items-center'>
+                    <a className={Style.button}>Give us a call</a>
+                </div>
+            </div>
         </nav>
     )
 }
